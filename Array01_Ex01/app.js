791bc6f1b40170e81f17d865f01eeb748eaf7838
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-02-27 10:43:15 +0700

    Add files via upload

---
+++ Array01_Ex01/app.js
@@ -0,0 +1,10 @@
+let myColor = ["Red", "Green", "White", "Black"];
+
+/*let data = "";
+for (i= 0; i < myColor.length; i++) {
+    data += myColor[i] + ", ";
+}
+document.write(data);
+*/
+
+document.write(myColor.toString());
